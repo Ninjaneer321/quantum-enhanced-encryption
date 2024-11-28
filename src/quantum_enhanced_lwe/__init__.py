fa6cd98b8d2c1c7629d1e2f5f2a45cd8176ceb2d
Matt Dixon <dxn000@gmail.com>
2024-11-28 16:58:50 -0500

    Add core module and update import paths for QuantumEnhancedLWE

---
+++ src/quantum_enhanced_lwe/__init__.py
@@ -0,0 +1,3 @@
+from .core.quantum_enhanced_lwe import QuantumEnhancedLWE
+
+__all__ = ['QuantumEnhancedLWE']
---
+++ src/quantum_enhanced_lwe/core/__init__.py
@@ -0,0 +1,4 @@
+from .quantum_enhanced_lwe import QuantumEnhancedLWE
+from .quantum_resonance_circuit import QuantumResonanceCircuit
+
+__all__ = ['QuantumEnhancedLWE', 'QuantumResonanceCircuit']
